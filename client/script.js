0292b839063460b621a8a101e26bba036b6bef0b
Arqestra <mshawdietrich@gmail.com>
2022-10-19 12:38:34 +0100

    API Scripts

--- client/script.js
+++ client/script.js
@@ -1,15 +1,29 @@
 const searchBtn = document.querySelector('#sbutton');
 const luckyBtn = document.querySelector('#lucky');
+const body = document.querySelector('body');
 
-searchBtn.addEventListener('click', getAllResults)
+searchBtn.addEventListener('click', getAllResults);
 luckyBtn.addEventListener('click', getRandomResult)
 
+
 function getAllResults(e) {
-    e.preventDefault();
-    window.location.replace('search.html')
     fetch('http://localhost:3000/results')
         .then(resp => resp.json())
-        .then(resp => {     })
+        .then(showMovies)
+}
+
+function showMovies(results) {
+    for (let m = 0; m < results.length; m++) {
+        const element = results[m];
+        const newH3 = document.createElement('h3');
+        newH3.textContent = `Name: ${element.title}`;
+        const newP = document.createElement('p');
+        newP.textContent = `Description: ${element.desc}`;
+        body.append(newH3);
+        body.append(newP);
+        
+    }
+    
 }
 
 function getRandomResult(e) {
